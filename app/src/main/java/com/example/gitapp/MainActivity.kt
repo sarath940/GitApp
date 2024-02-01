1d0b679dc3086e0c0d8a370cb3e02aa6862db575
Krishna Sarath Pentapati <sarath940@gmail.com>
2024-02-01 23:34:10 +1100

    commit 1

--- app/src/main/java/com/example/gitapp/MainActivity.kt
+++ app/src/main/java/com/example/gitapp/MainActivity.kt
@@ -8,12 +8,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("Sarath","Master change 1")
-        Log.d("Sarath","Master change 2")
-        Log.d("Sarath","Master change 3")
-        Log.d("sarath","Feature Commit 1")
-        Log.d("sarath","Feature Commit 2")
-        Log.d("sarath","Feature Commit 3")
-        Log.d("Sarath","Master change 3")
     }
 }
